aad69fb6ad085ee9028e9c91335d3de94549f6af
mashhura20204 <ibraimovamashhura0412@gmail.com>
2025-12-02 17:09:20 +0500

    Update and rename README.md to main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# mashq5_0211
+#6 - misol
+
+ uzunligi = lambda s:len(s)
+ print(uzunligi("salom"))
+# 7 - misol
+
+number = lambda x: x>= 0
+print(number(5))
+print(number(-4))
+print(number(0))
+# 8- misol
+
+uchburchak = lambda asos, balandlik: 0.5 * asos* balandlik
+print(uchburchak(10,6))
+print(uchburchak(5, 8))
+# 9-misol
+
+juft =  lambda x:x % 2 == 0
+print(juft(4))
+# 10 - misol
+
+katta = lambda x: x>10
+print(katta(2))
